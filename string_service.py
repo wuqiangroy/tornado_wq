95d68053ca087f92fee28376ed4cad584563d27b
wuqiangroy <wuqiangroy@live.com>
2017-02-13 23:04:06 +0800

    add string_service

--- string_service.py
+++ string_service.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+from tornado import httpserver, ioloop, options, web
+import textwrap
+
+
+class ReverseHandler(web.RequestHandler):
+    def get(self, input):
+        self.write(input[::-1])
+
+
+class WrapHandler(web.RequestHandler):
+    def post(self):
+        text = self.get_argument('text')
+        width = self.get_argument('width', 40)
+        self.write(textwrap.fill(text, int(width)))
+
+if __name__ == '__mian__':
+    options.parse_command_line()
+    app = web.Application(
+        handlers=[
+            (r'/reverse/(\w+)', ReverseHandler),
+            (r'/wrap', WrapHandler)
+        ]
+    )
+    http_server = httpserver.HTTPServer(app)
+    http_server.listen(options.options.port)
+    ioloop.IOLoop.instance().start()
