9b87e7537fe6a93f199cba7f5dacf746d31f00e2
wuqiangroy <wuqiangroy@live.com>
2017-02-23 08:21:18 +0800

    tweet_rate

--- twitter/tweet_rate.py
+++ twitter/tweet_rate.py
@@ -9,6 +9,7 @@ from tornado import httpserver, ioloop, options, web, httpclient
 
 options.define('port', default=8000, help='run this given port', type=int)
 
+
 class IndexHandler(web.RequestHandler):
     def get(self):
         query = self.get_argument('q')
@@ -25,6 +26,21 @@ class IndexHandler(web.RequestHandler):
             raw_oldest_tweet_at, '%a %d %b %Y %H:%M:%S + 0000')
         seconds_diff = time.mktime(now.timetuple()) - \
             time.mktime(oldest_tweet_at.timetuple())
+        tweet_per_second = float(result_count) / seconds_diff
+        self.write("""
+        <div style='text-align: center'>
+            <div style='font-size: 72px'>%s</div>
+            <div style='font-size: 144px'>%0.2f</div>
+            <div style='font-size: 24px'>tweets per second</div>
+        </div>""" % (query, tweet_per_second))
+
+if __name__ == '__main__':
+    options.parse_command_line()
+    app = web.Application(handlers=[
+        (r'/', IndexHandler)])
+    http_server = httpserver.HTTPServer(app)
+    http_server.listen(options.options.port)
+    ioloop.IOLoop.instance().start()
 
 
 
