98fd4e9ea66cf135133a575ecb8dcc9b0f6ee779
wuqiangroy <wuqiangroy@live.com>
2017-02-28 21:44:33 +0800

    add post function

--- wq/handlers/index.py
+++ wq/handlers/index.py
@@ -8,3 +8,8 @@ class IndexHandler(web.RequestHandler):
 
     def get(self):
         self.render('index.html')
+
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        self.write(username)
