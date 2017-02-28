74c2854bbdcf51f61f87c578b409c23fa70a16bc
wuqiangroy <wuqiangroy@live.com>
2017-02-28 21:13:54 +0800

    add js file

--- wq/statics/js/script.js
+++ wq/statics/js/script.js
@@ -1,3 +1,12 @@
 /**
  * Created by wuqia on 2017/2/28.
  */
+
+$(document).ready(function(){
+    alert("good");
+    $("#login").click(function(){
+        var user = $("#username").val();
+        var pwd = $("#password").val();
+        alert("username: "+user);
+    });
+});
