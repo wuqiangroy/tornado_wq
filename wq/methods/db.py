e47b5c12c856ac07d4baf966bf0e9ca5efa7d032
wuqiangroy <wuqiangroy@live.com>
2017-03-01 21:50:35 +0800

    full db

--- wq/methods/db.py
+++ wq/methods/db.py
@@ -11,3 +11,11 @@ import MySQLdb
 conn = MySQLdb.connect(host='localhost', user='root', passwd='1234',
                        db='wuqiangroy', port=3306, charaset='utf8')
 cur = conn.cursor()
+
+
+def select_table(table, column, condition, value):
+    sql = 'select' + column + 'from' + table + 'where' + condition + \
+        "='" + value + "'"
+    cur.execute(sql)
+    lines = cur.fetchall()
+    return lines
