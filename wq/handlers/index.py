5c63f44bbc229e544e2321cbe3ec6dc0f41e9fe7
wuqiangroy <wuqiangroy@live.com>
2017-03-01 22:07:45 +0800

    full post function

--- wq/handlers/index.py
+++ wq/handlers/index.py
@@ -2,6 +2,7 @@
 # _*_ coding:utf-8 _*_
 
 from tornado import web
+from ..methods.db import select_table
 
 
 class IndexHandler(web.RequestHandler):
@@ -12,4 +13,13 @@ class IndexHandler(web.RequestHandler):
     def post(self):
         username = self.get_argument('username')
         password = self.get_argument('password')
-        self.write(username)
+        user_infos = select_table(table='users', column='*',
+                                  condition='username', value=username)
+        if user_infos:
+            db_pwd = user_infos[0][2]
+            if db_pwd == password:
+                self.write('Welcome you:' + username)
+            else:
+                self.write('Sorry, your password is wrong!')
+        else:
+            self.write('This user is not exist.')
