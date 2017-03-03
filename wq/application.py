ec11016171f860d61eda577c11cb5a437759d3c3
wuqiangroy <wuqiangroy@live.com>
2017-03-03 21:12:07 +0800

    add cookie secret

--- wq/application.py
+++ wq/application.py
@@ -7,7 +7,8 @@ from url import url
 
 settings = {
     'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
-    'static_path': os.path.join(os.path.dirname(__file__), 'statics')
+    'static_path': os.path.join(os.path.dirname(__file__), 'statics'),
+    'cookie_secret': "Qdbk+l5DSz6XOw9zBCSYKA=="
 }
 
 application = web.Application(
