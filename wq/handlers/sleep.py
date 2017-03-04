d8b35b6ccd592361c2b9208cb9520a6b15233d61
wuqiangroy <wuqiangroy@live.com>
2017-03-04 20:58:06 +0800

    add synchronize

--- wq/handlers/sleep.py
+++ wq/handlers/sleep.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+import time
+from base import BaseHandler
+
+
+class SleepHandler(BaseHandler):
+
+    def get(self):
+        time.sleep(17)
+        self.render('sleep.html')
+
+
+class SeeHandler(BaseHandler):
+
+    def get(self):
+        self.render('see.html')
