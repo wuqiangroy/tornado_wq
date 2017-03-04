6134d4cad7084275bf130fa44c9eb8afdb5aaa33
wuqiangroy <wuqiangroy@live.com>
2017-03-04 21:22:09 +0800

    add asynchronous

--- wq/handlers/sleep.py
+++ wq/handlers/sleep.py
@@ -2,14 +2,29 @@
 # _*_ coding:utf-8 _*_
 
 import time
+from tornado import web, ioloop, gen
 from base import BaseHandler
 
 
 class SleepHandler(BaseHandler):
 
+    @web.asynchronous
     def get(self):
-        time.sleep(17)
+        ioloop.IOLoop.instance().add_timeout(time.time()+17, callback=
+                                             self.on_response)
+
+    def on_response(self):
         self.render('sleep.html')
+        self.finish()
+"""
+class SleepHandler(BaseHandler):
+
+    @gen.coroutine
+    def get(self):
+        yield gen.Task(ioloop.IOLoop.instance().add_timeout,
+                       time.time()+17)
+        self.render("sleep.html')
+"""
 
 
 class SeeHandler(BaseHandler):
